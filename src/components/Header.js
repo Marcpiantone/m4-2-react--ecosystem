c49f342aaec04c5299fafc220fbc906868663f76
“Marc <marc_piantone@live.fr>
2020-07-22 09:24:24 -0400

    Working header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,44 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const HeaderBox = styled.header`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
 
 const Logo = styled.p`
   font-size: 50px;
   font-weight: bolder;
-  margin: 40px;
 `;
+
+const Linkbox = styled.div`
+  justify-content: flex-end;
+  font-weight: bolder;
+  font-size: 20px;
+  a {
+    text-decoration: none;
+    margin: 10px;
+    color: rgb(153, 51, 255);
+  }
+
+  & a:hover {
+    color: rgb(255, 0, 127);
+    border-bottom: solid 2px;
+  }
+`;
+
 const Header = (props) => {
   return (
-    <header>
+    <HeaderBox>
       <Logo>Fruit Emporium</Logo>
-      <link></link>
-    </header>
+      <Linkbox>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </Linkbox>
+    </HeaderBox>
   );
 };
 
--- src/globalstyles.js
+++ src/globalstyles.js
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-body {font-family: "Raleway", sans-serif;}`;
+body {font-family: "Raleway", sans-serif; margin: 20px}`;
 
 export default GlobalStyle;
